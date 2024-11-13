7c61aa3ef874d2bfdf015afeda5e820fa06bac18
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-11-13 16:06:42 +0700

    feat: implementation definition route (#1)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,30 @@
 import { Stack } from "expo-router";
+import { Text } from "react-native";
 
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerTitle: "P3K" }} />
-      <Stack.Screen name="definisi" />
+      <Stack.Screen
+        name="definisi"
+        options={{
+          headerTransparent: true,
+          headerShown: true,
+          headerTitle: () => {
+            return (
+              <Text
+                style={{
+                  display: "none",
+                }}
+              ></Text>
+            );
+          },
+          headerStyle: {
+            backgroundColor: "rgba(0,0,0,0.5)",
+          },
+          headerTintColor: "white",
+        }}
+      />
     </Stack>
   );
 }
